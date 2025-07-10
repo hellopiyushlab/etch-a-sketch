8235e45c02d76dbcf8e0557d1cd92e7a85430d8e
Piyush <hellopiyush.lab@gmail.com>
2025-07-10 19:01:09 +0530

    added color picker

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ You can make pixel art on this.
 ### To DO
 - [x] Input options for number of boxes
 - [x] Ability to press-hold and color
-- [ ] Input color options
+- [x] Input color options
 - [ ] Eraser
 - [ ] Mobile Friendly
 - [ ] Make it look good
--- script.js
+++ script.js
@@ -1,45 +1,6 @@
 // sketch board is the div in very center
-sketchBoard = document.querySelector("#sketch-board");
-
-function createGrid(noOfBoxes) {
-    
-    // remove the old grid
-    const sketchBoard = document.getElementById('sketch-board');
-    while (sketchBoard.firstChild) {
-        sketchBoard.removeChild(sketchBoard.firstChild);
-    }
-
-    const dimension = Math.sqrt(noOfBoxes); // give the number of boxes on each dimension
-    const percent = 100 / dimension; // give the percentage for width / height
-
-    // use flex to generate rows, with each row being a flex box itself for columns
-
-    // loop for generating rows
-    for (let i = 0; i < dimension; i++) {
-        row = document.createElement("div");
-        row.classList.add("row");
-        row.style.height = `${percent}%`;
-        sketchBoard.appendChild(row);
-
-        // loop for generating columns / boxes within each row
-        for (let i = 0; i < dimension; i++) {
-            column = document.createElement("div");
-            column.classList.add("box");
-            column.style.width = `${percent}%`;
-            row.appendChild(column);
-        }
-    }
-    const boxes = document.querySelectorAll(".box"); // select all boxes, returns a NodeList
-    boxes.forEach(box => {
-    box.addEventListener("mouseenter", () => {
-            if (isMouseDown) {
-                box.style.backgroundColor = "black";
-            }
-        });
-    });
-}
-
-let noOfBoxes = 0;
+const colorPicker = document.getElementById("colorChoice");
+let noOfBoxes = 64;
 createGrid(noOfBoxes); // initial Grid
 
 const sixteenBit = document.querySelector("#sixteenBit");
@@ -59,6 +20,40 @@ let isMouseDown = false;
 document.addEventListener("mousedown", () => {isMouseDown = true;});
 document.addEventListener("mouseup", () => {isMouseDown = false;});
 
+function createGrid(noOfBoxes) {
+    
+    // remove the old grid
+    const sketchBoard = document.getElementById('sketch-board');
+    while (sketchBoard.firstChild) {
+        sketchBoard.removeChild(sketchBoard.firstChild);
+    }
 
-//summar of code above
-//so first we applied event listener to each box, which could've been done by a traditional function syntax and then each event listener calls a function (arrow function in this case, could've been a traditional syntax) and changes color
+    const dimension = Math.sqrt(noOfBoxes); // give the number of boxes on each dimension
+    const percent = 100 / dimension; // give the percentage for width / height
+
+    // use flex to generate rows, with each row being a flex box itself for columns
+
+    // loop for generating rows
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.height = `${percent}%`;
+        sketchBoard.appendChild(row);
+
+        // loop for generating columns / boxes within each row
+        for (let j = 0; j < dimension; j++) {
+            const column = document.createElement("div");
+            column.classList.add("box");
+            column.style.width = `${percent}%`;
+            row.appendChild(column);
+        }
+    }
+    const boxes = document.querySelectorAll(".box"); // select all boxes, returns a NodeList
+    boxes.forEach(box => {
+    box.addEventListener("mouseenter", () => {
+            if (isMouseDown) {
+                box.style.backgroundColor = colorPicker.value;
+            }
+        });
+    });
+}
