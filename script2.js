3d2545a03b23d70fdd61a75a01dd00950f4e90ed
Piyush <hellopiyush.lab@gmail.com>
2025-07-11 09:14:05 +0530

    New DOM

---
+++ script2.js
@@ -0,0 +1,59 @@
+// sketch board is the div in very center
+const colorPicker = document.getElementById("colorChoice");
+let noOfBoxes = 64;
+createGrid(noOfBoxes); // initial Grid
+
+const sixteenBit = document.querySelector("#sixteenBit");
+sixteenBit.addEventListener("click", () => {
+    noOfBoxes = 256;
+    createGrid(noOfBoxes); // regenerate grid
+});
+
+const thirtyTwoBit = document.querySelector("#thirtyTwoBit");
+thirtyTwoBit.addEventListener("click", () => {
+    noOfBoxes = 1024;
+    createGrid(noOfBoxes); // regenerate grid
+})
+
+// to color only when holding mouse button
+let isMouseDown = false;
+document.addEventListener("mousedown", () => {isMouseDown = true;});
+document.addEventListener("mouseup", () => {isMouseDown = false;});
+
+function createGrid(noOfBoxes) {
+    
+    // remove the old grid
+    const sketchBoard = document.getElementById('sketch-board');
+    while (sketchBoard.firstChild) {
+        sketchBoard.removeChild(sketchBoard.firstChild);
+    }
+
+    const dimension = Math.sqrt(noOfBoxes); // give the number of boxes on each dimension
+    const percent = 100 / dimension; // give the percentage for width / height
+
+    // use flex to generate rows, with each row being a flex box itself for columns
+
+    // loop for generating rows
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.height = `${percent}%`;
+        sketchBoard.appendChild(row);
+
+        // loop for generating columns / boxes within each row
+        for (let j = 0; j < dimension; j++) {
+            const column = document.createElement("div");
+            column.classList.add("box");
+            column.style.width = `${percent}%`;
+            row.appendChild(column);
+        }
+    }
+    const boxes = document.querySelectorAll(".box"); // select all boxes, returns a NodeList
+    boxes.forEach(box => {
+    box.addEventListener("mouseenter", () => {
+            if (isMouseDown) {
+                box.style.backgroundColor = colorPicker.value;
+            }
+        });
+    });
+}
